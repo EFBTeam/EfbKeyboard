7ed8f5f263fac03ee2c06a8959f8d77b23e4ffee
Richard Niland <Richard.Niland@aerlingus.com>
2025-07-30 23:01:58 +0100

    Add FloatingKeyboard project

---
+++ FloatingKeyboard/Application.xaml.vb
@@ -0,0 +1,21 @@
+﻿Imports WpfKb.Controls
+
+Class Application
+
+    ' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
+    ' can be handled in this file.
+
+    Protected Overrides Sub OnStartup(e As StartupEventArgs)
+        MyBase.OnStartup(e)
+
+        Dim Floater = New FloatingDocunetKeyboard() With {
+            .AreAnimationsEnabled = True,
+            .IsOpen = True,
+            .Width = 900,
+            .Height = 400,
+            .IsDragHelperAllowedToHide = True
+        }
+
+    End Sub
+
+End Class
